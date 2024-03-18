77c5b840e7b8ac43b199be106ded8c837e6c67b5
brett.baudin.consulting <brett.baudin.consulting@gmail.com>
2024-03-18 12:38:29 -0600

    Add speech-to-text and text-to-speech functionality

--- backend/src/routes/userRoutes.mjs
+++ backend/src/routes/userRoutes.mjs
@@ -94,33 +94,62 @@ router.post("/", validate(userSchema), async (req, res) => {
 });
 
 const cleanObject = (obj) => {
-  obj = obj.toObject ? obj.toObject({
-    versionKey: false, transform: (doc, ret) => {
-      Object.keys(ret).forEach(key => {
-        if (key.startsWith('_')) {
-          delete ret[key];
-        } else if (typeof ret[key] === 'object' && ret[key] !== null) {
-          ret[key] = cleanObject(ret[key]);
-        }
-      });
-    }
-  }) : obj;
+  // Check if the object is a Date instance or other non-plain objects you wish to preserve.
+  if (obj instanceof Date || !(obj instanceof Object) || obj === null) {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    // If obj is an array, apply cleanObject to each element.
+    return obj.map(cleanObject);
+  } else if (typeof obj === 'object') {
+    // If obj is a plain object, iterate its properties.
+    const cleaned = {};
+    Object.keys(obj).forEach((key) => {
+      if (!key.startsWith('_')) {
+        // Recursively apply cleanObject to the property value.
+        cleaned[key] = cleanObject(obj[key]);
+      }
+    });
+    return cleaned;
+  }
+  // Return the value directly if it's neither an object nor an array.
   return obj;
 };
 
 // GET route to fetch a user by userId
 router.get("/:userId", async (req, res) => {
   try {
-    const user = await User.findOne({ userId: req.params.userId });
+    const user = await User.findOne({ userId: req.params.userId }).lean(); // .lean() for performance if you're not modifying the document
     if (!user) {
       return res.status(StatusCodes.NOT_FOUND).send();
     }
+
+    // Add default textToSpeechModel if it's not present
+    if (!user?.settings?.textToSpeechModel) {
+      const defaultTextToSpeechModel = {
+        model_id: "tts-1",
+        vendor: "OpenAI",
+        voice_id: "shimmer",
+      };
+      user.settings.textToSpeechModel = defaultTextToSpeechModel;
+    }
+
+    // Add default speechToTextModel if it's not present
+    if (!user?.settings?.speechToTextModel) {
+      const defaultSpeechToTextModel = {
+        model_id: "tts-1",
+        vendor: "OpenAI",
+      };
+      user.settings.speechToTextModel = defaultSpeechToTextModel;
+    }    
+
     // Clean the user object before sending it
     const sanitizedUser = cleanObject(user);
     res.send(sanitizedUser);
   } catch (error) {
     logger.error(error);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error.message); // Send only the error message for better security practice
   }
 });
 
